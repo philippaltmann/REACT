15ce4f8119a912d1a5063ac04c827761b75fb428
Philipp <philipp.altmann@ifi.lmu.de>
2025-04-07 23:23:32 +0200

    Updated training to hyphi envs

--- src/train/train_model.py
+++ src/train/train_model.py
@@ -1,68 +1,48 @@
-import random
-
 import click
 import gymnasium as gym
-import hyphi_gym
-import stable_baselines3.common.base_class
-from class_resolver import Hint
+from hyphi_gym import named, register_envs, Monitor
 from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
 from stable_baselines3.common.logger import configure
-from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.monitor import Monitor as SB3M
+from constants import MODEL_PATH
+from stable_baselines3 import PPO, SAC
+from stable_baselines3.common.base_class import BaseAlgorithm
 
-from constants import TRAIN_LOG_PATH, MODEL_PATH
-from train.models import model_resolver
+
+class Evaluate(EvalCallback):      
+    def _on_step(self) -> bool:
+        continue_training = super()._on_step()
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0 and self.render:
+          self.eval_env.envs[0].save_video(f'{self.log_path[:-11]}eval-{self.n_calls}.gif')
+        return continue_training
 
 
 @click.command("train")
 @click.option("--env-name")
 @click.option("--name", default="train")
-@model_resolver.get_option("--model", as_string=True)
+@click.option("--model", default="PPO", type=click.Choice(["PPO", "SAC"]))
 @click.option("--render", is_flag=True)
 @click.option("--steps", default=10000)
-@click.option("--save-freq", default=5000)
+@click.option("--save-freq", default=50000)
 @click.option("--env-seed", default=42)
-def train(env_name: str, name: str, model: Hint[stable_baselines3.common.base_class.BaseAlgorithm], render: bool,
-          steps: int, save_freq: int, env_seed: int):
-    name = name + "_" + model
+def train(env_name: str, name: str, model, render: bool, steps: int, save_freq: int, env_seed: int):
+    register_envs(); save_freq = min(save_freq, steps)
+    log_dir = f'{env_name}/{name}_{model}'
+    save_path = MODEL_PATH.joinpath(log_dir)
 
-    if "FlatGrid" in env_name:
-        env = gym.make(**hyphi_gym.named(env_name), render_mode="3D")
-        policy = "MlpPolicy"
+    if env_name == "FetchReach": env_name += 'Targets' # Train on random targets / evaluate on random initial pos
+    render_mode = "blender" if "Grid" in env_name else "3D"
+    env = gym.make(**named(env_name), seed=env_seed, render_mode=render_mode) # , continue_task=True
 
-    elif "HoleyGrid" in env_name:
-        env_kwargs = hyphi_gym.named(env_name)
-        env = gym.make(env_kwargs["id"], level=env_kwargs["level"], sparse=env_kwargs["sparse"],
-                       detailed=env_kwargs["detailed"], explore=env_kwargs["explore"], random=env_kwargs["random"],
-                       seed=env_seed)
-        policy = "MlpPolicy"
+    eval_callback = Evaluate(
+        Monitor(SB3M(env), record_video=render), best_model_save_path=save_path,
+        log_path=save_path, eval_freq=save_freq, deterministic=True, render=render, verbose=1)
+    checkpoint_callback = CheckpointCallback(save_freq=save_freq, save_path=save_path)
 
-    elif env_name == "FetchReach":
+    model:BaseAlgorithm = eval(model)(policy="MlpPolicy", env=env, seed=env_seed)
 
-        if render:
-            env = gym.make('FetchReach-v2', max_episode_steps=50, render_mode="human")
-        else:
-            env = gym.make('FetchReach-v2', max_episode_steps=50)
-
-        policy = "MultiInputPolicy"
-    else:
-        raise ValueError("environment can only be FlatGrid, HoleyGrid or FetchReach")
-
-    eval_env = hyphi_gym.Monitor(env, record_video=True)
-    eval_callback = EvalCallback(eval_env, best_model_save_path=MODEL_PATH.joinpath(env_name + "/" + name),
-                                log_path=TRAIN_LOG_PATH.joinpath(env_name + "/" + name), eval_freq=save_freq,
-                                deterministic=True, render=render, verbose=1)
-    checkpoint_callback = CheckpointCallback(save_freq=save_freq, save_path=MODEL_PATH.joinpath(env_name + "/" + name))
-
-
-
-    # seed always creates the same holes
-    kwargs = {"policy": policy, "env": env, "seed": env_seed}
-    model = model_resolver.make(model, kwargs)
-    new_logger = configure(str(TRAIN_LOG_PATH.joinpath(env_name + "/" + name)), ["csv"])
+    new_logger = configure(str(save_path), ["csv"])
     model.set_logger(new_logger)
 
-    random.seed() # Such that the seed from the environment is not used!
-
     model.learn(total_timesteps=steps, callback=[checkpoint_callback, eval_callback])
-
     env.close()
