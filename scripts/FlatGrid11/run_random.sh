1d87edf1008453b58e78c0268e0b331006f558ea
Philipp <philipp.altmann@ifi.lmu.de>
2025-04-18 18:53:44 +0200

    Merged random and ablation scripts

--- scripts/FlatGrid11/run_random.sh
+++
@@ -1,3 +0,0 @@
-for SEED in 42 13 24 18 46 19 28 32 91 12; do 
-  react evo --env-name FlatGrid11 --saved-model train_PPO --checkpoint 35000 --name FlatGrid11 --seed $SEED --pop-size 10 --iterations 0 --encoding-length 6
-done
--- scripts/HoleyGrid11/run_random.sh
+++
@@ -1,3 +0,0 @@
-for SEED in 42 13 24 18 46 19 28 32 91 12; do 
-  react evo --env-name HoleyGrid11 --saved-model train_PPO --checkpoint 150000 --name HoleyGrid11 --seed $SEED --pop-size 10 --iterations 0 --encoding-length 6
-done
