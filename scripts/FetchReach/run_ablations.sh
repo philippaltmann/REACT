c93820d0928b16208c65b919fbc5e8958e90bc0f
Philipp <philipp.altmann@ifi.lmu.de>
2025-04-18 18:54:07 +0200

    Updated fetch scripts

---
+++ scripts/FetchReach/run_ablations.sh
@@ -0,0 +1,16 @@
+BASE="--env-name FetchReach --saved-model train_SAC --pop-size 10 --encoding-length 9"
+REACT="--iterations 40 --is-elitist --crossover 0.75 --mutation 0.5"
+
+for S in "50000 50k" "100000 100k" "200000 200k"; do
+  set -- $S # $1 -> CHECKPOINT, $2 -> NAME
+  for SEED in 42 13 24 18 46 19 28 32 91 12; do 
+    echo "Running $SEED"
+    react evo $BASE $REACT --checkpoint $1 --name Fetch$2 --seed $SEED --w1 1 --w2 0 --w3 0 --w4 0  # REACT_G
+    # react evo $BASE $REACT --checkpoint $1 --name Fetch$2 --seed $SEED --w1 0 --w2 1 --w3 1 --w4 1  # REACT_D
+    react evo $BASE $REACT --checkpoint $1 --name Fetch$2 --seed $SEED --w1 1 --w2 1 --w3 1 --w4 0  # REACT P
+    react evo $BASE $REACT --checkpoint $1 --name Fetch$2 --seed $SEED --w1 0 --w2 1 --w3 0 --w4 0  # REACT_L
+    react evo $BASE $REACT --checkpoint $1 --name Fetch$2 --seed $SEED --w1 0 --w2 0 --w3 1 --w4 0  # REACT_C
+    react evo $BASE $REACT --checkpoint $1 --name Fetch$2 --seed $SEED --w1 0 --w2 0 --w3 0 --w4 0  # REACT_F
+    react evo $BASE --checkpoint $1 --name Fetch$2  --seed $SEED --iterations 0  # Random 
+  done
+done
--- scripts/FetchReach/run_react.sh
+++ scripts/FetchReach/run_react.sh
@@ -1,6 +1,6 @@
-for S in "100000 100k" "3000000 3M" "5000000 5M"; do
-  set -- $S # $1 -> STEPS, $2 -> STEPS_NAME
+for S in "50000 50k" "100000 100k" "200000 200k"; do
+  set -- $S # $1 -> CHECKPOINT, $2 -> NAME
   for SEED in 42 13 24 18 46 19 28 32 91 12; do 
-    react evo --env-name FetchReach --saved-model train_sac --checkpoint $1 --name Fetch$2 --seed $SEED --pop-size 30 --iterations 1000 --encoding-length 9 --plot-frequency 100 --is-elitist --crossover 0.75 --mutation 0.5
+    react evo --env-name FetchReach --saved-model train_SAC --checkpoint $1 --name Fetch$2 --seed $SEED --pop-size 10 --iterations 40 --encoding-length 9 --is-elitist --crossover 0.75 --mutation 0.5
   done
 done
