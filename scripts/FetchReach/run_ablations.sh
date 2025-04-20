810b26b2a6a6cb1ab09154126cb36a5cc7433db0
Philipp <philipp.altmann@ifi.lmu.de>
2025-04-20 14:31:16 +0200

    Updated Fetch scripts

--- scripts/FetchReach/run_ablations.sh
+++ scripts/FetchReach/run_ablations.sh
@@ -1,7 +1,7 @@
 BASE="--env-name FetchReach --saved-model train_SAC --pop-size 10 --encoding-length 9"
 REACT="--iterations 40 --is-elitist --crossover 0.75 --mutation 0.5"
 
-for S in "50000 50k" "100000 100k" "200000 200k"; do
+for S in "50000 50k" "100000 100k" "150000 150k"; do
   set -- $S # $1 -> CHECKPOINT, $2 -> NAME
   for SEED in 42 13 24 18 46 19 28 32 91 12; do 
     echo "Running $SEED"
--- scripts/FetchReach/run_train.sh
+++ scripts/FetchReach/run_train.sh
@@ -1,4 +1,4 @@
-for S in "50000 50k" "100000 100k" "200000 200k"; do
+for S in "50000 50k" "100000 100k" "150000 150k"; do
   set -- $S # $1 -> CHECKPOINT, $2 -> NAME
   for SEED in 42 13 24 18 46 19 28 32 91 12; do 
     react run --env-name FetchReach --saved-model train_SAC --checkpoint $1 --name Fetch$2 --seed $SEED 
